ff6fdf9c2b21fbeb86779c2c7050542a4c68ecbd
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-10-17 00:46:35 +0600

    added new component; conditionally change button color and text

---
+++ src/components/Dance.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+
+// Define the emits type
+const emit = defineEmits<{ (event: "dance", value: boolean): void }>();
+
+// Use ref to make isDancing reactive
+const isDancing = ref(false);
+
+const buttonText = computed(() =>
+	isDancing.value ? "Stop Dance" : "Start Dance"
+);
+
+// Create a computed property for button class
+const buttonClass = computed(() => {
+    const baseClass = "border px-6 py-2 text-xl font-bold rounded-full transition-all duration-500";
+    const colorClass = isDancing.value ? "border-red-900 text-red-900 hover:bg-red-900 hover:text-white" : "border-teal-900 text-teal-900 hover:bg-teal-900 hover:text-white";
+    return `${baseClass} ${colorClass}`;
+});
+
+// Handle dance button click
+const handleDance = () => {
+	isDancing.value = !isDancing.value; // Toggle dancing state
+	emit("dance", isDancing.value); // Emit the dancing state
+};
+</script>
+
+<template>
+	<div class="w-full flex items-center justify-center">
+		<button
+			@click="handleDance"
+			:class="buttonClass"
+		>
+			{{ buttonText }}
+		</button>
+	</div>
+</template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,23 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import Dance from "./Dance.vue";
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ heading: string }>();
 
-const count = ref(0)
+const dancingClass = ref("");
+
+const updateDanceState = (isDancing: boolean) => {
+	dancingClass.value = isDancing ? "animate-bounce" : "animate-none";
+};
+
+console.log(dancingClass);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
-    starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank">Vue Docs Scaling up Guide</a>.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+	<h1
+		:class="`h-[33vh] text-3xl font-bold flex items-center justify-center ${dancingClass}`"
+	>
+		{{ heading }}
+	</h1>
+	<Dance @dance="updateDanceState" />
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
